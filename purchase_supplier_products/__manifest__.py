71cccd2ffcf688d0ba68e6cde0ee23ff9fb1ae04
fwyaime <47220219+fwyaime@users.noreply.github.com>
2020-04-15 15:00:15 +0200

    [ADD] purchase

---
+++ purchase_supplier_products/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Products for Supplier",
+    'author': "Jason Pindat, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+
+    'category': 'Purchase',
+    'version': '11.0.1.0',
+
+    'depends': [
+        'purchase',
+    ],
+
+    'data': [
+        'views/res_partner.xml',
+    ],
+}
