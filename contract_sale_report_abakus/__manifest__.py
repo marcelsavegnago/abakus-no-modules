6104f4420328bd94fa605ae64eedbb4b18858659
fwyaime <47220219+fwyaime@users.noreply.github.com>
2020-04-15 16:11:46 +0200

    [ADD] contract_sale_report

---
+++ contract_sale_report_abakus/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Order Report for Contract - AbAKUS",
+    'version': '11.0.1.0',
+    'depends': [
+        'sale_order_improvements',
+    ],
+    'author': "ABAKUS IT-SOLUTIONS",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Sale',
+    'data': [
+        'reports/contract_sale_report.xml',
+    ],
+}
