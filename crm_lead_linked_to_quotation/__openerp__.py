74ee97ee63b4c7e0b9e69eb2ae9a79b43be85cad
fwyaime <47220219+fwyaime@users.noreply.github.com>
2020-04-15 12:17:53 +0200

    [ADD] crm_lead modules

---
+++ crm_lead_linked_to_quotation/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Opportunities to/from Quotations",
+    'version': '11.0.1.0',
+    'depends': [
+        'sale_crm',
+    ],
+    'author': "ABAKUS IT-SOLUTIONS",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Sale/CRM',
+    'data': [
+        'view/crm_lead_view.xml',
+        'view/sale_order_view.xml',
+    ],
+}
