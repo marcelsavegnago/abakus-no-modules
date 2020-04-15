8ee9934a9dc24d514775f63745e91c63e183c5cd
fwyaime <47220219+fwyaime@users.noreply.github.com>
2020-04-15 11:26:59 +0200

    [ADD] account_asset_management_analytic

---
+++ account_asset_management_analytic/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# (c) AbAKUS IT Solutions
+{
+    "name": "Account Asset Management Analytic",
+    'version': '10.0.1.0.0',
+    'author': "ABAKUS IT-SOLUTIONS",
+    'license': 'AGPL-3',
+    "website": "http://www.abakusitsolutions.eu",
+    "depends": [
+        "account",
+        "account_asset",
+    ],
+    "category": "Accounting",
+    "data": [
+        "views/account_asset_view.xml",
+    ],
+}
